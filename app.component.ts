66acb741fdb899fc9e94bff19499e78f906ebb28
Gabriela Isabel Alvarado <75093925+emigabriella@users.noreply.github.com>
2024-08-19 19:21:59 -0600

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CatalogoComponent } from "./catalogo/catalogo.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CatalogoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'gabriela_ortiz';
+}
---
+++ catalogo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalogo.component.html',
+  styleUrl: './catalogo.component.css'
+})
+export class CatalogoComponent {
+  nombre1:string = "Daniel Trujillo ";
+  nombre2 ="Tania Rosales";
+  nombre3 = "Cristian Flores";
+}
+
+
